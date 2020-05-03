556c33581382936f651e3e4e4606dc813e0c0426
Abhilash Rath <39529016+AbhilashRath@users.noreply.github.com>
2020-05-03 20:40:14 +0530

    Create Ransom_Note.java

---
+++ Ransom_Note.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+            if(hm.containsKey(magazine.charAt(i))){
+                hm.put(magazine.charAt(i),hm.get(magazine.charAt(i))+1);
+            }else{
+                hm.put(magazine.charAt(i),1);
+            }
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            if(!hm.containsKey(ransomNote.charAt(i))){
+                return false;
+            }else{
+                if(hm.get(ransomNote.charAt(i))==0){
+                    return false;
+                }else{
+                    hm.put(ransomNote.charAt(i),hm.get(ransomNote.charAt(i))-1);
+                }
+            }
+        }
+        return true;
+    }
+}
